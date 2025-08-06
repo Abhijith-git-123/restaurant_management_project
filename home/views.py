8b5571c06d443ecfaa3296fad1a8c4d343539be9
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-06 21:22:41 +0530

    22353

--- home/views.py
+++ home/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
-from .models import Res
+from .models import Restaurant
 
 # Create your views here.
+
+def home_main(request):
+    data = Restaurant.objects.first()
+
+    context = {
+        'rest_name':data.name
+    }
+    return render(request, 'home.html',context)
