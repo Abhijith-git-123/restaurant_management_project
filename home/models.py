713540b22f2a3c9c7bf968a2bd723eded764ceed
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-06 21:19:54 +0530

    22353

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length = 200)
+
+
+    def _str__(self):
+        return self.name
