d75752bac915be9311bf61131a96ba3c7694433f
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-07 16:14:28 +0530

    24459

--- home/views.py
+++ home/views.py
@@ -41,3 +41,18 @@ def contact_us(request):
         'email':data.email
     }
     return render(request, 'contact.html')
+
+
+def list_menu(request):
+    menu_items = [
+        {'name':'Chicken Biriyani', 'Price':250},
+        {'name': 'Paneer Butter Masala', 'Price': 180},
+        {'name': 'Veg Biriyani', 'Price': 220},
+        {'name': 'chicken Tikka', 'Price': 300}
+    ]
+
+    context = {
+        'rest_name': 'Thiruvonam Restaurant',
+        'menu_items': menu_items,
+    }
+    return render(request,'menu.html',context)
