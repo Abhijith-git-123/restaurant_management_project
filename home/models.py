3853187fe7e69354530bec6a7b58d6d3cfd90d03
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-07 15:59:56 +0530

    24461

--- home/models.py
+++ home/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length = 200)
     phone = models.CharField(max_length=30)
+    email = models.CharField(max_length=50)
 
 
     def _str__(self):
