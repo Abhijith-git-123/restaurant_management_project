a541551d17b2f5b67e920c6ea5c25cca0ab2d70f
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-08 16:51:10 +0530

    26074

--- home/models.py
+++ home/models.py
@@ -10,3 +10,12 @@ class Restaurant(models.Model):
 
     def _str__(self):
         return self.name
+
+
+
+class feedback(models.Model):
+    comment = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.submitted_at
