512d82f518c70826fdee8d3424420a1d38c41c67
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-12 22:19:10 +0530

    31215

--- home/models.py
+++ home/models.py
@@ -36,3 +36,13 @@ class feedback(models.Model):
 
     def __str__(self):
         return self.submitted_at
+
+
+class contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
