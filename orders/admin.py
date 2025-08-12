565c5dbc6d325c18b680d7330f143292a7a6a70c
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-12 17:31:50 +0530

    30611

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models omport *
+
 # Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'total_amount', 'order_status', 'created_at')
+    search_fields = ('customer_username', 'order_status')
+    list_filter = ('order_status', 'created_at')
+    filter_horizontal = ('order_items',)
+
+
+admin.site.Register()
