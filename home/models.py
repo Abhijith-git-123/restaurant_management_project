286350361a512f45082479a54863e15af0cd15ae
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-12 16:44:41 +0530

    30608

--- home/models.py
+++ home/models.py
@@ -1,6 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete = moels.CASCADE, related_name = "profile")
+
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length = 200)
