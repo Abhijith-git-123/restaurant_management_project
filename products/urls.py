e68153b50c38d53f87b6deecd791fd21b65fc4fe
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-18 20:20:57 +0530

    34045

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
-    path('add-item',views.add_item)
+    path('add-item',views.add_menu_item,name = 'add_item'),
+    path('add-item-post',views.add_menu_item_post,name='add-item-post')
 ]
