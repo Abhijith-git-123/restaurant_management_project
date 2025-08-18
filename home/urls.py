dea6edd89a39d49e54b58502c58a5951a47b0937
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-18 21:37:08 +0530

    34047

--- home/urls.py
+++ home/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('about',views.about_page, name = 'about'),
     path('404',views.404_not found, name = '404'),
     path('contact_us',views.contact_us,name="contact_us"),
+    path('contact_us_post',views.contact_us_post,name="contact_us_post"),
     path('reservation',views.reservation,name = "reservation"),
     path('feedback',views.feedback, name = 'feedback')
 
