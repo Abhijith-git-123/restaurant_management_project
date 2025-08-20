6bf217729bdfac965070a1efb5085da07cb51f64
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-20 17:08:17 +0530

    35478

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -130,3 +130,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gamil.com'
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'myrestaurant@gmail.com'
+
+EMAIL_HOST_PASSWORD = 'restaurant@123'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
