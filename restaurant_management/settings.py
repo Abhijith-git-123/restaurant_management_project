75089de77fadae424552178ec8ec75d90c51e1d2
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-25 21:54:03 +0530

    39073

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -45,6 +45,11 @@ INSTALLED_APPS = [
     'orders',
 ]
 
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
