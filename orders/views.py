572b8b6736b66cd7b594d027b00688073107e199
Abhijith-git-123 <159511649+Abhijith-git-123@users.noreply.github.com>
2025-08-27 16:59:45 +0530

    39075

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import random
+from django.shortcuts import render
+
+def order_confirmation(request):
+  
+
+    order_number = random.randint(1000, 9999)
+
+    context = {
+        "rest_name":"Thiruvonam Retaurant",
+        "order_number": order_number
+
+    }
+    return render(request, "order_confirmation.html",context)
